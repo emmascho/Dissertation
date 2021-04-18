8f3edebdf2af62ce78ade3ec4679ca934d429fb9
Emma Schoenmakers <emmaschoenmakers@Emmas-MacBook-Pro.local>
2021-04-18 16:40:45 +0100

    Made the graph prettier with outline and labels

--- GraphRCode/LandCoverArea_1980-2020.R
+++ GraphRCode/LandCoverArea_1980-2020.R
@@ -12,18 +12,19 @@ p2 <- ggplot(area, aes(x=Year, y=Area, fill=LandClass, group=Year)) +
   facet_wrap(~LandClass, scale="free")
 
 
-plot(p2)
+
 
 hist <- ggplot(area, aes(x = Year, y = Area, fill = LandClass, group=Year,l=30)) +
-    geom_bar(stat = "identity", position = "dodge",width=10) + 
+    geom_bar(stat = "identity", position = "dodge",width=10, color="black") + 
   scale_fill_manual(values=c("#645BBF", "#2E7E9E","#A37838", "#DB891F", "#FFCC00"),
                     labels=c('Agriculture','Aquaculture','Mangrove','Other Forest','Urban'))+
     scale_x_continuous(breaks = c(1980,2000,2020)) +
     labs(x = "Decade", y = "Area Size (m2) \n", fill="Land Cover Type") + 
-  theme(panel.grid = element_blank(), 
-        axis.text = element_text(size = 8), 
-        axis.title = element_text(size = 10), 
-        plot.title = element_text(size = 8, hjust = 0.5, face = "bold"))+
+  geom_text(aes(label=Area), vjust=2.6, size=2)+
+  theme(panel.grid = element_blank(),
+        axis.text = element_text(size = 10), 
+        axis.title = element_text(size = 14), 
+        plot.title = element_text(size = 10, hjust = 0.5, face = "bold"))+
     facet_wrap(~LandClass, scale="free")
 plot(hist)
 
