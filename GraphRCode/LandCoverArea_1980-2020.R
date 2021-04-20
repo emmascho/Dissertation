642f30144d89c753048135592119896336c3f3ac
Emma Schoenmakers <emmaschoenmakers@Emmas-MacBook-Pro.local>
2021-04-20 10:56:16 +0100

    Finalised classification outputs overview by adding legends to barchart

--- GraphRCode/LandCoverArea_1980-2020.R
+++ GraphRCode/LandCoverArea_1980-2020.R
@@ -6,26 +6,23 @@ library(viridis)
 
 area <- Area_R
 
-# one box per variety
-p2 <- ggplot(area, aes(x=Year, y=Area, fill=LandClass, group=Year)) + 
-  geom_boxplot() +
-  facet_wrap(~LandClass, scale="free")
 
-
-
-
-hist <- ggplot(area, aes(x = Year, y = Area, fill = LandClass, group=Year,l=30)) +
-    geom_bar(stat = "identity", position = "dodge",width=10, color="black") + 
+hist <- ggplot(area, aes(x = Year, y = Area, fill = LandClass, group=Year,l=30,label=sprintf("0.1f", round(Area, digits = 1)))) +
+    geom_bar(stat = "identity", position = "dodge",width=13) + 
   scale_fill_manual(values=c("#645BBF", "#2E7E9E","#A37838", "#DB891F", "#FFCC00"),
                     labels=c('Agriculture','Aquaculture','Mangrove','Other Forest','Urban'))+
     scale_x_continuous(breaks = c(1980,2000,2020)) +
-    labs(x = "Decade", y = "Area Size (m2) \n", fill="Land Cover Type") + 
-  geom_text(aes(label=Area), vjust=2.6, size=2)+
+    labs(x = "\n Decade\n", y = "Area Size (ha) \n", fill="Land Cover Type") + 
+  geom_text(aes(label=Area), vjust=-0.4, size=3, colour="black")+
   theme(panel.grid = element_blank(),
+        legend.position = "bottom",
         axis.text = element_text(size = 10), 
-        axis.title = element_text(size = 14), 
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size=10),
+        legend.title= element_text(size=12, face="bold"),
         plot.title = element_text(size = 10, hjust = 0.5, face = "bold"))+
-    facet_wrap(~LandClass, scale="free")
+    facet_wrap(~LandClass, scale="free")+ 
+  theme(strip.text.x = element_text(size = 12, colour = "black", face="bold"))
 plot(hist)
 
 
